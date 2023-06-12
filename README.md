9eb4482b6ee3fef4f6fb02a97c89894e502c383d
11mones <mones_leader@hotmail.com>
2023-06-12 23:20:32 +0000

    done but i have to change readme after deployment

---
+++ README.md
@@ -0,0 +1,26 @@
+## LAB - Class 16 Serverless Functions
+
+**Project: Capital finder**
+
+
+**Author: Mones saeed**
+
+**Setup**
+
+-Install packages : 
+
+
+        pip i
+
+How to run the endpoints ? 
+
+
+        python pythonic_garage_band.py
+
+**Tests**
+
+
+
+How to run tests?
+
+          pytest band.py
--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,27 +1,44 @@
 from http.server import BaseHTTPRequestHandler
-import requests 
 from urllib import parse
- 
+import urllib.request
+import json
 
-    
 class handler(BaseHTTPRequestHandler):
- 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    list_of_dif=[]
-    message="testing"
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_list = parse.parse_qsl(url_components.query)
-    my_dict = dict(query_list)
-    
 
-   
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        list_of_def = []
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_list = parse.parse_qsl(url_components.query)
+        my_dict = dict(query_list)
 
+        if 'country' in my_dict:
+            country = my_dict.get('country')
+            country_url = f"https://restcountries.com/v3.1/name/{country}"
 
+            response = urllib.request.urlopen(country_url)
+            data = json.loads(response.read())
 
+            for element in data:
+                d = element['name']['common']
+                message = "The capital of {} is {}".format(country, d)
+                list_of_def.append(message)
 
-    self.wfile.write(message.encode())
-    return
+        if 'capital' in my_dict:
+            capital = my_dict.get('capital')
+            capital_url = f"https://restcountries.com/v3.1/capital/{capital}"
+
+            response = urllib.request.urlopen(capital_url)
+            data = json.loads(response.read())
+
+            for element in data:
+                country = element['name']['common']
+                message = "{} is the capital of {}".format(capital, country)
+                list_of_def.append(message)
+
+        message = '\n'.join(list_of_def)
+        self.wfile.write(message.encode())
+        return
