013b6458124a43eb6cbb6ceb34351c00bf9d0101
11mones <mones_leader@hotmail.com>
2023-06-12 19:52:22 +0000

    having a problem with requests

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,10 +1,27 @@
 from http.server import BaseHTTPRequestHandler
+import requests 
+from urllib import parse
  
+
+    
 class handler(BaseHTTPRequestHandler):
  
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    list_of_dif=[]
+    message="testing"
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_list = parse.parse_qsl(url_components.query)
+    my_dict = dict(query_list)
+    
+
+   
+
+
+
+
+    self.wfile.write(message.encode())
+    return
